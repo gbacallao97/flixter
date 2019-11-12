4d0a39dad005a0aa457c9ee3d3b37d72d50baffe
Gabriel <gabrielcoding97@gmail.com>
2019-11-12 06:31:33 +0000

    carrierwave, figaro, and aws installed for heroku deployment to work w images

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_053427) do
+ActiveRecord::Schema.define(version: 2019_11_12_051238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(version: 2019_11_08_053427) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["section_id"], name: "index_lessons_on_section_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "sections", force: :cascade do |t|
