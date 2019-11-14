f20f54c2cb2207757cac030b827f652da849d149
Gabriel <gabrielcoding97@gmail.com>
2019-11-14 05:22:56 +0000

    added stripe to take users payment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,16 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+    before_action :require_authorized_for_current_lesson, only: [:show]
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      redirect_to course_path(current_lesson.section.course), alert: 'you must be enrolled'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
